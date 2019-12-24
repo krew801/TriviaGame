3e07c10ac27a3db35614f0579e5db8755cca8d93
Alex Smith <alsmith@instructure.com>
2019-12-23 23:17:04 -0700

    fixed route to JS

--- javascript/app.js
+++ javascript/app.js
@@ -7,6 +7,7 @@ startButton.addEventListener('click', startGame)
 startGame = () => {
 startButton.classList.add('hide')
 questionContainerElement.classList.remove('hide')
+setNextQuestion()
 }
 
 setNextQuestion = () =>{
@@ -17,9 +18,7 @@ selectAnswer = () => {
 
 }
 
-$(document).ready(() => {
-
-    const questions = [
+const questions = [
         {
             prompt: "What is the date setting of the original Call of Duty?",
             choice: ["World War 2", "The Civil War", "World War 1", "Vietnam War"],
